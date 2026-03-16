909f5471f50c65b429dac82eefa9cdfb7be91038
Its-shreyasingh <ssshreya1210@gmail.com>
2026-03-16 19:41:37 +0530

    Setup of the service layer

---
+++ project/src/main/java/com/project/blog/Service/BlogService.java
@@ -0,0 +1,29 @@
+package com.project.blog.Service;
+import com.project.blog.Models.*;
+import java.util.*;
+import org.springframework.stereotype.Service;
+
+import com.project.blog.Repository.PostRepository;
+import com.project.blog.Repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BlogService {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public Post createPost(Long userId, Post post)
+    {
+        User user=userRepository.findById(userId)
+        .orElseThrow(()->new RuntimeException("User not found"));
+        post.setAuthor(user);
+        return postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts()
+    {
+        return postRepository.findAll();
+    }
+}
